58db650ccf7a7e9404d7967ebbd0bb12498baf23
Frederick Ollinger <fred.ollinger@seescan.com>
2015-07-21 08:58:25 -0700

    Using a layout.

--- HelloJava.java
+++ HelloJava.java
@@ -4,8 +4,15 @@ public class HelloJava{
     public static void main ( String[] args ){
         // System.out.println("Hello, Java");
 	JFrame frame = new JFrame( "Hello, Java" );
+    JPanel listPane = new JPanel();
+    listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+	//listPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
 	JLabel label = new JLabel( "Hello, Java", JLabel.CENTER );
-	frame.getContentPane().add(label);
+	JLabel label2 = new JLabel( "Hello, Again", JLabel.CENTER );
+	frame.add(listPane);
+	listPane.add(label);
+	listPane.add(label2);
+    //frame.getContentPane().add(label);
 	frame.setSize( 300, 300 );
 	frame.setVisible( true );
     }
