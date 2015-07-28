63daaf672ff80f7f6993a712988fab224cbee1cf
Fred Ollinger <follinge@gmail.com>
2015-07-27 19:27:27 -0700

    Now we have workable AttributePicker with all the stats setup up.

--- AttributePicker.java
+++ AttributePicker.java
@@ -1,5 +1,12 @@
 package com.fredollinger;
 import javax.swing.*;
 
-class AttributePicker extends JSpinner {
+class AttributePicker extends JPanel {
+    AttributePicker(String str){
+        setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+        JLabel label = new JLabel(str);
+        JSpinner spinner = new JSpinner();
+        add(label);
+        add(spinner);
+    } // END AttributePicker()
 } // END class Attribute
--- StatsGenerater.java
+++ StatsGenerater.java
@@ -5,15 +5,25 @@ public class StatsGenerater{
     public static void main ( String[] args ){
 	JFrame frame = new JFrame( "Hello, Java" );
         JPanel listPane = new JPanel();
-        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.LINE_AXIS));
 	JLabel label = new JLabel( "Total Stats:", JLabel.CENTER );
 	JLabel label2 = new JLabel( "15", JLabel.CENTER );
-	AttributePicker spinner = new AttributePicker( );
+	AttributePicker strPicker = new AttributePicker( "STR" );
+	AttributePicker dexPicker = new AttributePicker( "DEX" );
+	AttributePicker conPicker = new AttributePicker( "CON" );
+	AttributePicker intPicker = new AttributePicker( "INT" );
+	AttributePicker wisPicker = new AttributePicker( "WIS" );
+	AttributePicker chaPicker = new AttributePicker( "CHA" );
 
 	frame.add(listPane);
 	listPane.add(label);
 	listPane.add(label2);
-	listPane.add(spinner);
+	listPane.add(strPicker);
+	listPane.add(dexPicker);
+	listPane.add(conPicker);
+	listPane.add(intPicker);
+	listPane.add(wisPicker);
+	listPane.add(chaPicker);
 	frame.setSize( 300, 300 );
 	frame.setVisible( true );
     } // END main
